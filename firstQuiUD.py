7b0034d5bb0acd44e71df6e1a2845b7eac998be5
“@Kevine12” <ikevine2016@gmail.com>
2023-10-31 16:09:32 +0200

    adding data

--- firstQuiUD.py
+++ firstQuiUD.py
@@ -18,20 +18,20 @@ Welcome to Python3!
 print(snake_string * how_many_snakes)  # Use the variable directly, not as a string 'how_many_snakes'
 
 
-num=int(float(input('Enter a number :')) )
+# num=int(float(input('Enter a number :')) )
 
-print('hello'*num)
+# print('hello'*num)
 
-names = input("Enter names separated by commas: ").title().split(",")
-assignments = input("Enter assignment counts separated by commas: ").split(",")
-grades = input("Enter grades separated by commas: ").split(",")
+# names = input("Enter names separated by commas: ").title().split(",")
+# assignments = input("Enter assignment counts separated by commas: ").split(",")
+# grades = input("Enter grades separated by commas: ").split(",")
 
-message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
-submit before you can graduate. You're current grade is {} and can increase \
-to {} if you submit all assignments before the due date.\n\n"
+# message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+# submit before you can graduate. You're current grade is {} and can increase \
+# to {} if you submit all assignments before the due date.\n\n"
 
-for name, assignment, grade in zip(names, assignments, grades):
-    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
+# for name, assignment, grade in zip(names, assignments, grades):
+#     print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
 
 
 #     While True:
