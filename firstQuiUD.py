d62ff7326477e8d526a6a93669eda3b71badf862
“@Kevine12” <ikevine2016@gmail.com>
2023-10-31 11:19:53 +0200

    :adding first test python

---
+++ firstQuiUD.py
@@ -0,0 +1,18 @@
+# Write a script of your own
+# Open up a brand new empty file in your text editor, name it and save it in the place where you're keeping the files for your Python learning. Put the following code into it.:"how_many_snakes = 1
+how_many_snakes = 3  # Change the number to the desired count of snakes
+
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+print(snake_string * how_many_snakes)  # Use the variable directly, not as a string 'how_many_snakes'
