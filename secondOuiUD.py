1b9a098b23f772b3697aed6d27dc38435d293c0a
“@Kevine12” <ikevine2016@gmail.com>
2023-11-01 08:44:23 +0200

    access another file

--- secondOuiUD.py
+++ secondOuiUD.py
@@ -1,3 +1,18 @@
-import thirdOuiUD
+import thirdOuiUD  as tq
 print(2+5)
-print(thirdOuiUD.num)
+print(tq.num)
+
+
+
+scores = [88, 92, 79, 93, 85]
+
+mean = tq.mean(scores)
+curved = tq.add_five(scores)
+
+mean_c = tq.mean(curved)
+
+print("Scores:", scores)
+print("Original Mean:", mean, " New Mean:", mean_c)
+
+print(__name__)
+print(tq.__name__)
