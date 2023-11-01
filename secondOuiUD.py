8b34be23e06e23d18dcd62b228e7203eb4aa361a
“@Kevine12” <ikevine2016@gmail.com>
2023-11-01 09:09:45 +0200

    access another file

--- secondOuiUD.py
+++ secondOuiUD.py
@@ -16,3 +16,15 @@ print("Original Mean:", mean, " New Mean:", mean_c)
 
 print(__name__)
 print(tq.__name__)
+
+# It's your turn to import and use the math module. Use the math module to calculate e to the power of 3. And assign the answer to result.
+
+# Refer to the math module's documentation to find the function you need!
+
+
+import math
+
+# calculate e to the power of 3 using the math module
+result = math.exp(3)
+
+print(result)  # This will display the result of e raised to the power of 3
