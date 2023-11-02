c3584a92850c8523fa971d308325d77394ec5f83
“@Kevine12” <ikevine2016@gmail.com>
2023-11-02 17:19:59 +0200

    adding data

--- firstQuiUD.py
+++ firstQuiUD.py
@@ -353,3 +353,20 @@ class Pants:
 
 #     def calculate_discount(self, discount):
 #         return self.price * (1 - discount / 2)
+
+# Distribution class
+class Distribution:
+    def __init__(self, mu=0, sigma=1):
+        """ Generic distribution class for calculating and 
+        visualizing a probability distribution.
+
+        Attributes:
+            mean (float) representing the mean value of the distribution
+            stdev (float) representing the standard deviation of the distribution
+            data_list (list of floats) a list of floats extracted from the data file
+
+            """
+
+        self.mean = mu
+        self.stdev = sigma
+        self.data = []
