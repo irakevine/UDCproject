30d82b490703df28bfb6e57b376d4ab0609a8a4e
“@Kevine12” <ikevine2016@gmail.com>
2023-11-02 17:25:36 +0200

    adding data

--- firstQuiUD.py
+++ firstQuiUD.py
@@ -560,3 +560,52 @@ class Distribution:
 #       self.stdev = self.calculate_stddev(sample)
 
 # ```
+
+
+# Inheritance with the Gaussian Class
+# To give another example of inheritance, take a look at the code in this Jupyter notebook. The Gaussian distribution code is refactored into a generic Distribution class and a Gaussian distribution class. Read through the code in this Jupyter notebook to see how the code works.
+
+# The Distribution class takes care of the initialization and the read_data_file method. Then the rest of the Gaussian code is in the Gaussian class. You'll later use this Distribution class in an exercise at the end of the lesson.
+
+# Run the code in each cell of this Jupyter notebook. This is a code demonstration, so you do not need to write any code.
+
+class Distribution:
+    
+    def __init__(self, mu=0, sigma=1):
+    
+        """ Generic distribution class for calculating and 
+        visualizing a probability distribution.
+    
+        Attributes:
+            mean (float) representing the mean value of the distribution
+            stdev (float) representing the standard deviation of the distribution
+            data_list (list of floats) a list of floats extracted from the data file
+            """
+        
+        self.mean = mu
+        self.stdev = sigma
+        self.data = []
+
+
+    def read_data_file(self, file_name):
+    
+        """Function to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute.
+                
+        Args:
+            file_name (string): name of a file to read from
+        
+        Returns:
+            None
+        
+        """
+            
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+        file.close()
+    
+        self.data = data_list
