72cb9b673cc7b08fe579f4cddd30271f13108756
“@Kevine12” <ikevine2016@gmail.com>
2023-11-02 16:23:05 +0200

    adding data

--- firstQuiUD.py
+++ firstQuiUD.py
@@ -322,3 +322,5 @@ class Pants:
     
 
     # Like in the previous question, assume the average weight of an American adult male is 180 pounds with a standard deviation of 34 pounds. The distribution of weights follows a normal distribution. What is the probability that a man weighs somewhere between 120 and 155 pounds = 0.19?
+
+    # Now consider a Binomial distribution. Assume that 15% of the population is allergic to cats. If you randomly select 60 people for a medical trial, what is the probability that 7 of those people are allergic to cats = 0.12?
