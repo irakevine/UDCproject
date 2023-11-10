6006d2c5b3b807e3c865010964c8c1212e2846b0
“@Kevine12” <ikevine2016@gmail.com>
2023-11-10 10:21:30 +0200

    Manipulating ndarrays

---
+++ exercise.py
@@ -0,0 +1,1 @@
+# Create a 5 x 5 ndarray X with consecutive integers from 1 to 25 (inclusive). Afterwards use Boolean indexing to pick out only the odd numbers in the array and assign the result to Y.
