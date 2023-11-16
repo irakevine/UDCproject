2b797e0a062189f3ad6f3f52a2fe64c8af07a1b0
“@Kevine12” <ikevine2016@gmail.com>
2023-11-16 16:34:25 +0200

    Matplotlib and Seaborn Part 1

--- learning.py
+++ learning.py
@@ -4,3 +4,11 @@
 # Refer [here](https://docs.python.org/2/library/string.html#format-string-syntax) for more details
 tick_names = ['{:0.2f}'.format(v) for v in tick_props]
 tick_names
+
+
+# Example 2 - Step 4. Plot the bar chart, with new x-tick labels 
+
+sb.countplot(data=pkmn_types, y='type', color=base_color, order=type_order);
+# Change the tick locations and labels
+plt.xticks(tick_props * n_pokemon, tick_names)
+plt.xlabel('proportion');
