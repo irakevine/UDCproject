7ec47985e3657b506e1c2943cabc8f531f0578c6
“@Kevine12” <ikevine2016@gmail.com>
2023-12-07 16:47:11 +0200

    adding data

--- exercise.py
+++ exercise.py
@@ -55,3 +55,10 @@ def softmax(L):
 
 
 # Now, time to shine! Let's code the formula for cross-entropy in Python. As in the video, Y in the quiz is for the category, and P is the probability.
+
+
+
+def cross_entropy(Y, P):
+    Y = np.float_(Y)
+    P = np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
